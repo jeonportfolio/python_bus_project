18006a393774ca938090e35ed2dd356c667c36f3
jeons <etoile929@naver.com>
2024-05-20 23:49:41 +0900

    버스 노선 표시 완료

--- bus/index.py
+++ bus/index.py
@@ -30,3 +30,37 @@ def get_station_by_pos():
 
     response = requests.get(url, params=params)
     return response.content, response.status_code   
+
+@app.route("/getRouteByStation", methods=['POST'])
+def get_route_by_station():
+    # POST 요청에서 JSON 데이터 추출
+    data = request.json
+    arsId = data['arsId']
+    # 외부 API로부터 데이터 가져오기
+    url = " http://ws.bus.go.kr/api/rest/stationinfo/getRouteByStation"
+    params = {
+        'serviceKey': "nP48y1O6pt9zw6eUekNNCyiPVjqcECZPhaTgI49TzaXttCTKtRzhyBtVyZD//RVja9A5jf/lSMPEEeRN2KYOLA==",
+        'arsId': arsId,
+        'radius': "500"  #반경 (단위: 미터)
+    }
+
+    response = requests.get(url, params=params)
+    return response.content, response.status_code  
+
+
+@app.route("/getRoutePath", methods=['POST'])
+def get_route_path():
+    # POST 요청에서 JSON 데이터 추출
+    data = request.json
+    busRouteId = data['busRouteId']
+
+    # 외부 API로부터 데이터 가져오기
+    url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getRoutePath"
+    params = {
+        'serviceKey': "nP48y1O6pt9zw6eUekNNCyiPVjqcECZPhaTgI49TzaXttCTKtRzhyBtVyZD//RVja9A5jf/lSMPEEeRN2KYOLA==",
+        'busRouteId': busRouteId,
+        'radius': "500"  #반경 (단위: 미터)
+    }
+
+    response = requests.get(url, params=params)
+    return response.content, response.status_code          
